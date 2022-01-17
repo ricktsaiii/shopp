945ec936802f98458c7358a3fdbd9749f0309f90
ricktsaiii <ricktsaiii@gmail.com>
2022-01-17 09:14:07 +0800

    更新

--- src/main.js
+++ src/main.js
@@ -11,6 +11,11 @@ import 'sweetalert2/dist/sweetalert2.min.css'
 import mixin from './mixin.js'
 import ImgInputer from 'vue-img-inputer'
 import 'vue-img-inputer/dist/index.css'
+import VueGtag from 'vue-gtag'
+
+Vue.use(VueGtag, {
+  config: { id: 'G-4CX7G37SCW' }
+})
 
 Vue.component('ImgInputer', ImgInputer)
 Vue.use(VueSweetalert2)
